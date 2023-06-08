1303661d36e6e00538e45dd25622c1bfe5334ea1
rindrarolando <rindrarolando7@gmail.com>
2023-06-08 09:48:32 +0300

    School Model and configuration adding

--- school_demo/school_api/models.py
+++ school_demo/school_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    percent_african_american = models.FloatField()
+
+    def __str__(self):
+        return self.name
