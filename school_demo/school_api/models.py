3304cffae53f3b09a1632962ed122b15473f7caf
rindrarolando <rindrarolando7@gmail.com>
2023-06-08 14:51:52 +0300

    removing school model (useless)

--- school_demo/school_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class School(models.Model):
-    name = models.CharField(max_length=100)
-    state = models.CharField(max_length=2)
-    percent_african_american = models.FloatField()
-
-    def __str__(self):
-        return self.name
